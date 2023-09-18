723dbc815598d21f182c97aa5870123da2c49619
Lucas Payr <orasund@gmail.com>
2023-09-18 05:51:49 +0200

    Update to elm 0.19.0

--- tests/NimTest.elm
+++ tests/NimTest.elm
@@ -1,51 +1,75 @@
 module NimTest exposing (..)
 
-import Minimax exposing (..)
-
-import Expect exposing (Expectation)
+import Expect
+import MinMaxSearch exposing (..)
 import Test exposing (..)
+
 
 suite : Test
 suite =
-  describe "NimTest.elm"
-  
-  [ 
-    test "Nim Test" <|
-      \() ->
-        Expect.equal (nodeEquals (Minimax.minimax moveFunc valueFunc possibleMovesFunc startPosition maxDepth) (Just 1)) True
+    describe "NimTest.elm"
+        [ test "Nim Test" <|
+            \() ->
+                Expect.equal
+                    (nodeEquals
+                        (MinMaxSearch.minimax
+                            { apply = moveFunc
+                            , eval = valueFunc
+                            , possibleMoves = possibleMovesFunc
+                            , start = startPosition
+                            , searchDepth = maxDepth
+                            }
+                        )
+                        (Just 1)
+                    )
+                    True
+        ]
 
-  ]
 
 nodeEquals : Node Int Int -> Maybe Int -> Bool
 nodeEquals node move =
-  (node.move == move)
+    node.move == move
+
 
 moveFunc : Node Int Int -> Int -> Int
 moveFunc node taken =
-  node.position - taken
+    node.position - taken
+
 
 valueFunc : Node Int Int -> Int
 valueFunc node =
-  if (node.position == 0) then
-    if (node.depth % 2 == 0) then
-      10
+    if node.position == 0 then
+        if node.depth |> modBy 2 |> (==) 0 then
+            10
+
+        else
+            0
+
     else
-      0
-  else
-    5
+        5
+
 
 possibleMovesFunc : Node Int Int -> List Int
 possibleMovesFunc node =
-  case node.position of
-    0 -> []
-    1 -> [1]
-    2 -> [1, 2]
-    _ -> [1, 2, 3]
-    
+    case node.position of
+        0 ->
+            []
+
+        1 ->
+            [ 1 ]
+
+        2 ->
+            [ 1, 2 ]
+
+        _ ->
+            [ 1, 2, 3 ]
+
+
 startPosition : Int
 startPosition =
-  5
+    5
+
 
 maxDepth : Int
 maxDepth =
-  6
+    6
--- tests/SimpleTreeTest.elm
+++ tests/SimpleTreeTest.elm
@@ -1,34 +1,83 @@
 module SimpleTreeTest exposing (..)
 
-import Minimax exposing (..)
-
-import Expect exposing (Expectation)
+import Expect
+import MinMaxSearch exposing (..)
 import Test exposing (..)
+
 
 suite : Test
 suite =
-  describe "SimpleTreeTes.elm"
-  
-  [ 
-    test "MiniMax Test Depth 4." <|
-      \() ->
-        Expect.equal (nodeEquals (Minimax.minimax moveFunc valueFunc possibleMovesFunc "" 4) (Just '0') (Number 4)) True
+    describe "SimpleTreeTes.elm"
+        [ test "MiniMax Test Depth 4." <|
+            \() ->
+                Expect.equal
+                    (nodeEquals
+                        (MinMaxSearch.minimax
+                            { apply = moveFunc
+                            , eval = valueFunc
+                            , possibleMoves = possibleMovesFunc
+                            , start = ""
+                            , searchDepth = 4
+                            }
+                        )
+                        (Just '0')
+                        (Number 4)
+                    )
+                    True
+        , test "MiniMax Test Depth 2." <|
+            \() ->
+                Expect.equal
+                    (nodeEquals
+                        (MinMaxSearch.minimax
+                            { apply = moveFunc
+                            , eval = valueFunc
+                            , possibleMoves = possibleMovesFunc
+                            , start = ""
+                            , searchDepth = 2
+                            }
+                        )
+                        (Just '0')
+                        (Number 4)
+                    )
+                    True
+        , test "MiniMax Test Depth 1." <|
+            \() ->
+                Expect.equal
+                    (nodeEquals
+                        (MinMaxSearch.minimax
+                            { apply = moveFunc
+                            , eval = valueFunc
+                            , possibleMoves = possibleMovesFunc
+                            , start = ""
+                            , searchDepth = 1
+                            }
+                        )
+                        (Just '1')
+                        (Number 5)
+                    )
+                    True
+        , test "MiniMax Test Depth 2. No possible moves" <|
+            \() ->
+                Expect.equal
+                    (nodeEquals
+                        (MinMaxSearch.minimax
+                            { apply = moveFunc
+                            , eval = valueFunc
+                            , possibleMoves = possibleMovesFuncEmpty
+                            , start = ""
+                            , searchDepth = 2
+                            }
+                        )
+                        Nothing
+                        Neg_Inf
+                    )
+                    True
+        ]
 
-  , test "MiniMax Test Depth 2." <|
-      \() ->
-        Expect.equal (nodeEquals (Minimax.minimax moveFunc valueFunc possibleMovesFunc "" 2) (Just '0') (Number 4)) True
 
-  , test "MiniMax Test Depth 1." <|
-      \() ->
-        Expect.equal (nodeEquals (Minimax.minimax moveFunc valueFunc possibleMovesFunc "" 1) (Just '1') (Number 5)) True
-
-  , test "MiniMax Test Depth 2. No possible moves" <|
-      \() ->
-        Expect.equal (nodeEquals (Minimax.minimax moveFunc valueFunc possibleMovesFuncEmpty "" 2) Nothing Neg_Inf) True
-  ] 
 
 --                  4 (5)                         max
---                 /  \ 
+--                 /  \
 --                /    \
 --               /      \
 --              /        \
@@ -47,74 +96,169 @@ suite =
 --/ \   |    |   / \   / \  / \  |   / | \
 --8 5   6    4   1 2   3 5  6 2  3   5 7 1        max
 
+
 nodeEquals : Node String Char -> Maybe Char -> IntegerExt Int -> Bool
 nodeEquals node move value =
-  (node.move == move) && (node.value == value)
+    (node.move == move) && (node.value == value)
+
 
 moveFunc : Node String Char -> Char -> String
 moveFunc node move =
-  node.position ++ String.fromChar move
+    node.position ++ String.fromChar move
+
 
 valueFunc : Node String Char -> Int
 valueFunc node =
-  case node.position of
-    ""     -> 5
+    case node.position of
+        "" ->
+            5
 
-    "0"    -> 3
-    "1"    -> 5
+        "0" ->
+            3
 
-    "00"   -> 6
-    "01"   -> 4
-    "10"   -> 3
-    "11"   -> 1
-    "12"   -> 2
+        "1" ->
+            5
 
-    "000"  -> 5
-    "001"  -> 6
-    "010"  -> 4
-    "011"  -> 1
-    "100"  -> 3
-    "110"  -> 2
-    "120"  -> 3
-    "121"  -> 1
+        "00" ->
+            6
 
-    "0000" -> 8
-    "0001" -> 5 
-    "0010" -> 6 
-    "0100" -> 4 
-    "0110" -> 1 
-    "0111" -> 2
-    "1000" -> 3
-    "1001" -> 5 
-    "1100" -> 6 
-    "1101" -> 2 
-    "1200" -> 3 
-    "1210" -> 5 
-    "1211" -> 7 
-    "1212" -> 1
-    _ -> 0
+        "01" ->
+            4
+
+        "10" ->
+            3
+
+        "11" ->
+            1
+
+        "12" ->
+            2
+
+        "000" ->
+            5
+
+        "001" ->
+            6
+
+        "010" ->
+            4
+
+        "011" ->
+            1
+
+        "100" ->
+            3
+
+        "110" ->
+            2
+
+        "120" ->
+            3
+
+        "121" ->
+            1
+
+        "0000" ->
+            8
+
+        "0001" ->
+            5
+
+        "0010" ->
+            6
+
+        "0100" ->
+            4
+
+        "0110" ->
+            1
+
+        "0111" ->
+            2
+
+        "1000" ->
+            3
+
+        "1001" ->
+            5
+
+        "1100" ->
+            6
+
+        "1101" ->
+            2
+
+        "1200" ->
+            3
+
+        "1210" ->
+            5
+
+        "1211" ->
+            7
+
+        "1212" ->
+            1
+
+        _ ->
+            0
+
 
 possibleMovesFuncEmpty : Node String Char -> List Char
-possibleMovesFuncEmpty node =
-  []
+possibleMovesFuncEmpty _ =
+    []
+
 
 possibleMovesFunc : Node String Char -> List Char
 possibleMovesFunc node =
-  case node.position of
-    ""    -> ['0', '1']
-    "0"   -> ['0', '1']
-    "00"  -> ['0', '1']
-    "000" -> ['0', '1']
-    "001" -> ['0']
-    "01"  -> ['0', '1']
-    "010" -> ['0']
-    "011" -> ['0', '1']
-    "1"   -> ['0', '1' , '2']
-    "10"  -> ['0']
-    "100" -> ['0', '1']
-    "11"  -> ['0']
-    "110" -> ['0', '1']
-    "12"  -> ['0', '1' ]
-    "120" -> ['0']
-    "121" -> ['0', '1' , '2']
-    _ -> []
+    case node.position of
+        "" ->
+            [ '0', '1' ]
+
+        "0" ->
+            [ '0', '1' ]
+
+        "00" ->
+            [ '0', '1' ]
+
+        "000" ->
+            [ '0', '1' ]
+
+        "001" ->
+            [ '0' ]
+
+        "01" ->
+            [ '0', '1' ]
+
+        "010" ->
+            [ '0' ]
+
+        "011" ->
+            [ '0', '1' ]
+
+        "1" ->
+            [ '0', '1', '2' ]
+
+        "10" ->
+            [ '0' ]
+
+        "100" ->
+            [ '0', '1' ]
+
+        "11" ->
+            [ '0' ]
+
+        "110" ->
+            [ '0', '1' ]
+
+        "12" ->
+            [ '0', '1' ]
+
+        "120" ->
+            [ '0' ]
+
+        "121" ->
+            [ '0', '1', '2' ]
+
+        _ ->
+            []
